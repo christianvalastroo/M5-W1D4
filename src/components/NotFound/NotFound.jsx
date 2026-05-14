38e7750de4845a46e908163900e68cf9a9cef456
Christian Valastro <christianvalastro@icloud.com>
2026-05-15 00:42:44 +0200

    Migliora pagina non trovata

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,12 +1,20 @@
-import { Container } from 'react-bootstrap';
-import Alert from 'react-bootstrap/Alert';
+import { Container, Button } from "react-bootstrap"
+import Alert from "react-bootstrap/Alert"
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
     return (
-        <Container>
-            <Alert className='text-center p-4'>
-                404 - Pagina non trovata
+        <Container className="text-center py-5">
+
+            <Alert variant="danger" className="p-4">
+                <h1>404</h1>
+                <p>Pagina non trovata</p>
+
+                <Button as={Link} to="/" variant="dark">
+                    Torna alla Home
+                </Button>
             </Alert>
+
         </Container>
     )
 }
