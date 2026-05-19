459355ddfe3e03f5a1f7e52471038d2fb8029e9d
Christian Valastro <christianvalastro@icloud.com>
2026-05-19 23:14:35 +0200

    Aggiunge test per il titolo di CommentArea

---
+++ src/components/CommentArea/CommentArea.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import CommentArea from "./CommentArea"
+import { ThemeProvider } from "../../context/ThemeHome/ThemeHome"
+
+describe("CommentArea component", () => {
+
+    it("should render comment area title", () => {
+
+        render(
+            <ThemeProvider>
+                <CommentArea selected="123" />
+            </ThemeProvider>
+        )
+
+        const title = screen.getByText(/recensioni del libro/i)
+
+        expect(title).toBeInTheDocument()
+    })
+})
